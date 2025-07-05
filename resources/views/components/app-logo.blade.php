4d96a2455d40e12f26e14f7a8c9bbb782e6c9d50
Silvio Martins <silviosmsantos@gmail.com>
2025-07-05 13:54:28 -0300

    first commit in the new projetc

---
+++ resources/views/components/app-logo.blade.php
@@ -0,0 +1,6 @@
+<div class="flex aspect-square size-10 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
+    <x-app-logo-icon class="size-10 fill-current text-white dark:text-black" />
+</div>
+<div class="ms-1 grid flex-1 text-start text-lg">
+    <span class="dark:text-sky-600 mb-0.5 truncate leading-tight font-semibold">Catalogi;</span>
+</div>
